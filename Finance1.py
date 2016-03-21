5437e04fe853881d7c561834c9353f37b370dfa6
Seananigans <sph99@comcast.net>
2016-03-20 22:28:10 -0400

    Added functions to retrieve data for Adj Close

--- Finance1.py
+++ Finance1.py
@@ -1,5 +1,28 @@
 import matplotlib.pyplot as plt
+import os
 import pandas as pd
+
+def symbol_to_path(symbol, base_dir="data"):
+	"""Return CSV file path given ticker symbol."""
+	return os.path.join(base_dir, "{}.csv".format(str(symbol)))
+
+def get_data(symbols, dates):
+	"""Read stock data (adjusted close) for given symbols from CSV files."""
+	df = pd.DataFrame(index=dates)
+	if 'SPY' not in symbols:  # add SPY for reference if absent
+		symbols.insert(0, "SPY")
+	
+	for symbol in symbols:
+		df_symbol = pd.read_csv(symbol_to_path(symbol), 
+								index_col="Date", 
+								parse_dates=True, 
+								usecols=['Date','Adj Close'], 
+								na_values=['nan'])
+		df_symbol = df_symbol.rename(columns={"Adj Close":symbol})
+		df = df.join(df_symbol, how="inner")
+		if symbol=="SPY":
+			df = df.dropna(subset=['SPY'])
+	return df
 
 def get_max_close(symbol):
 	"""Return the maximum closing value for stock indicated by symbol
@@ -20,7 +43,7 @@ def plot_multiple(data, *args):
 def start_end(start, end):
 	return pd.date_range(start, end)
 
-def read_and_join(df_date_range, *args):
+def read_and_join(df1, *args):
 	for symbol in list(args):
 		df_symbol = pd.read_csv("data/{}.csv".format(symbol), 
 								index_col="Date", 
@@ -28,10 +51,9 @@ def read_and_join(df_date_range, *args):
 								usecols=['Date','Adj Close'], 
 								na_values=['nan'])
 		df_symbol = df_symbol.rename(columns={"Adj Close":symbol})
-		print df_date_range.head()
-		df_date_range.join(df_symbol, how="inner")
-	return df_date_range
-
+		df1 = df1.join(df_symbol, how="inner")
+	return df1
+		
 def test_run():
 	df = pd.read_csv('data/AAPL.csv')
 	print df.head()
@@ -52,7 +74,7 @@ def test_run():
 # 						na_values=['nan'] )
 	df1 = df1.dropna()
 # 	df1 = df1.join(dfSPY, how="inner")
-	print read_and_join(df1, "SPY")
+	print read_and_join(df1, "SPY", "HCP", "AAPL","IBM")
 	
 if __name__ == "__main__":
 	test_run()

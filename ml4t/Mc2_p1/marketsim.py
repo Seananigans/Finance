9ad210a5e944323ef8570558db9199f209d4fa4b
Seananigans <sph99@comcast.net>
2016-03-28 22:56:39 -0400

    Copied Example from website in description
    http://quantsoftware.gatech.edu/MC2-Project-1
    
    Still having trouble with testing the test case

--- ml4t/Mc2_p1/marketsim.py
+++ ml4t/Mc2_p1/marketsim.py
@@ -7,42 +7,77 @@ import os
 from util import get_data, plot_data
 
 def compute_portvals(orders_file = "./orders/orders.csv", start_val = 1000000):
-	# this is the function the autograder will call to test your code
-	# TODO: Your code here
-	orders = pd.read_csv(orders_file, index_col="Date", 
-						parse_dates=True, na_values=['nan'])
-	print orders
-	# In the template, instead of computing the value of the portfolio, we just
-	# read in the value of IBM over 6 months
-# 	start_date = dt.datetime(2008,1,1)
-# 	end_date = dt.datetime(2008,6,1)
-	start_date = pd.to_datetime(orders.iloc[0].name)
-	end_date = pd.to_datetime(orders.iloc[-1].name)
-	symbols = list(orders.Symbol.unique())
-# 	portvals = get_data(['IBM'], pd.date_range(start_date, end_date))
-	portvals = get_data(symbols, pd.date_range(start_date, end_date))
-# 	portvals = portvals[['IBM']]  # remove SPY
-	for i in range(orders.shape[0]):
-		sym = orders.iloc[i].Symbol
-		date = pd.to_datetime(orders.iloc[i].name)
-		price = portvals.ix[date, sym]
-		stock_order = orders.iloc[i].Order
-		n_shares = orders.iloc[i].Shares
-		if stock_order = "BUY"
-			n_shares*price
-		else:
-			n_shares*price
-		
-		
-	
-	return portvals
+    # this is the function the autograder will call to test your code
+    # TODO: Your code here
+    # 1 Read CSV into trades array
+    trades = pd.read_csv(orders_file, index_col="Date", 
+                        parse_dates=True, na_values=['nan'])
+    print trades
+
+    # 2 Scan trades for symbols
+    symbols = list(trades.Symbol.unique())
+    # 3 Scan trades for dates
+    start_date = pd.to_datetime(trades.iloc[0].name)
+    end_date = pd.to_datetime(trades.iloc[-1].name)
+    
+    # 4 Read in data
+    portvals = get_data(symbols, pd.date_range(start_date, end_date))
+    portvals = portvals[symbols]  # remove SPY
+    
+    # 5 Scan trades to update cash
+    on_hand = portvals.copy()
+    on_hand[:] = 0.0
+    for i in range(trades.shape[0]):
+        sym = trades.iloc[i].Symbol
+        date = pd.to_datetime(trades.iloc[i].name)
+        print date
+        price = portvals.ix[date, sym]
+        stock_order = trades.iloc[i].Order
+        n_shares = trades.iloc[i].Shares
+        if stock_order == "BUY":
+            on_hand.ix[date, sym] = -n_shares*price
+        else:
+            on_hand.ix[date, sym] = n_shares*price
+    on_hand = np.sum(on_hand, axis=1)
+    
+    for i in range(on_hand.shape[0]):
+        if i==0:
+            on_hand.iloc[i] += start_val
+        else:
+            on_hand.iloc[i] += on_hand[i-1]
+    
+    # 6 Scan trades to create ownership array
+    portfolio = portvals.copy()
+    portfolio[:] = 0.
+    for i in range(trades.shape[0]):
+        sym = trades.iloc[i].Symbol
+        date = pd.to_datetime(trades.iloc[i].name)
+        price = portvals.ix[date, sym]
+        stock_order = trades.iloc[i].Order
+        n_shares = trades.iloc[i].Shares
+        if stock_order == "BUY":
+            portfolio.ix[date, sym] = n_shares
+        else:
+            portfolio.ix[date, sym] = -n_shares
+    # portfolio = np.sum(portfolio, axis=1)
+#   temp = portfolio.copy()
+    for i in range(1,portfolio.shape[0]):
+        portfolio.iloc[i] += portfolio.iloc[i-1]
+    portfolio = portfolio*portvals
+    portfolio = np.sum(portfolio, axis=1)
+    
+    # 7 Scan cash and value to create total fund value
+    portvals = portfolio + on_hand
+    
+    return portvals
 
 def test_code():
     # this is a helper function you can use to test your code
     # note that during autograding his function will not be called.
     # Define input parameters
 
-    of = "./orders/orders2.csv"
+#     of = "./orders/orders2.csv"
+    of = "./orders/orders-test.csv"
     sv = 1000000
 
     # Process orders

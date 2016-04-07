aec5348a3102ab759be2cb8e170b7b08877102c0
Seananigans <sph99@comcast.net>
2016-04-06 22:21:46 -0400

    Completed market sim.
    Added a data frame simply to record updates to current stock ownership.

--- ml4t/Mc2_p1/marketsim1.py
+++ ml4t/Mc2_p1/marketsim1.py
@@ -12,14 +12,16 @@ def compute_portvals(orders_file = "./orders/orders.csv", start_val = 1000000):
 	# 1 Read CSV into trades array
 	trades = pd.read_csv(orders_file, index_col="Date", 
 						parse_dates=True, na_values=['nan'])
+	print trades
 	
 	# 2 Scan trades for symbols
 	symbols = list(trades.Symbol.unique())
-	
 	# 3 Scan trades for dates
 	start_date = pd.to_datetime(trades.index.min())
 	end_date = pd.to_datetime(trades.index.max())
+	
 	dates = pd.date_range(start_date, end_date)
+
 	
 	# 4 Read in data
 	portvals = get_data(symbols, dates)
@@ -31,10 +33,36 @@ def compute_portvals(orders_file = "./orders/orders.csv", start_val = 1000000):
 	# 6 Scan trades to create ownership array
 	portfolio = portvals.copy()
 	portfolio[:] = 0.
+	own = portvals.copy()
+	own[:] = 0#np.nan
+# 	own = trades.join(own, how="right")
+# 	signal = (own["Order"]=="SELL")*-1.0 + (own["Order"]=="BUY")
+# 	for symbol in own.columns.values:
+# 		if symbol not in ["Symbol", "Order", "Shares"]:
+# 			own[symbol] = own["Shares"]*(own["Symbol"]==symbol) * signal
+# 	own = own.drop(["Symbol", "Order", "Shares"],1)
+# 	own[own+1.0==1.0]=np.nan
+# 	current_val=start_val
+# 	cash = pd.DataFrame(index=dates)
+# 	cash["Cash"]= 0.0
+# 	cash = cash.join(own, how="right")
+# 	cash = cash["Cash"]
+# 	for i in range(own.shape[0]):
+# 		cash.iloc[i] = current_val - np.sum(own*portvals, axis=1).fillna(0).iloc[i]
+# 		current_val = cash.iloc[i]
+# 	print cash
 
 	# 5 + 6
-	shorts = longs = 0
-	time_own = portfolio.iloc[0].copy()
+	shorts = longs = pd.DataFrame()
+	port = trades.join(portvals, how="right")#.fillna(method="ffill")
+	for symbol in port.columns.values:
+		if symbol not in ["Symbol", "Order", "Shares"]:
+			port[symbol] = port[symbol]*(port["Symbol"]==symbol)
+##	print port
+##	print "++++"*44
+	prev_trade = own.iloc[0].copy()
+	time_own = own.iloc[0].copy()
+	print "Currently Own:\n{}".format(time_own)
 	cash= start_val
 	for i in range(trades.shape[0]):
 		sym = trades.iloc[i].Symbol
@@ -42,54 +70,63 @@ def compute_portvals(orders_file = "./orders/orders.csv", start_val = 1000000):
 		price = portvals.ix[date, sym]
 		stock_order = trades.iloc[i].Order
 		n_shares = trades.iloc[i].Shares
-
-		#Store old portfolio in case order is rejected
-		longs0 = longs
-		shorts0 = shorts
-		cash0 = cash
-		time_own0 = time_own.copy()
-		
-		if stock_order == "BUY":
-			time_own[sym] += n_shares
-			cash -= n_shares*price
-		else:
-			time_own[sym] -= n_shares
-			cash += n_shares*price
-
-		longs = sum((time_own[time_own>0]*portvals.ix[date,:]).fillna(0.0))
-		shorts = sum((time_own[time_own<0]*portvals.ix[date,:]).fillna(0.0))
-		leverage = (longs + abs(shorts)) / (longs - abs(shorts) + cash)
-		print "Longs:\t\t{}".format(longs)
-		print "Shorts:\t\t{}".format(shorts)
-		print "Cash:\t\t{}".format(cash)
-		print "Leverage:\t{}".format(leverage)
-		
-		if leverage >= 2.0:
-                        print "LEVERAGE EXCEEDED"
-                        print "Potential Leverage:\t{}".format(leverage)
-                        print "REJECTING ORDER"
-			shorts, longs, cash = shorts0, longs0, cash0
-			time_own = time_own0.copy()
-                        print "Reset Leverage:\t{}".format((longs + abs(shorts)) / (longs - abs(shorts) + cash))
-			continue
 		
 		if stock_order == "BUY":
 			on_hand.ix[date, sym] -= n_shares*price
 			portfolio.ix[date, sym] += n_shares
+			time_own[sym] += n_shares
 		else:
 			on_hand.ix[date, sym] += n_shares*price
 			portfolio.ix[date, sym] -= n_shares
+			time_own[sym]-=n_shares
+		print "Currently Own:\n{}".format(time_own)
+		longs = sum((time_own[time_own>0]*portvals.ix[date,:]).fillna(0.0))
+		print "Longs:\n{}".format(longs)
+		shorts = sum((time_own[time_own<0]*portvals.ix[date,:]).fillna(0.0))
+		print "Shorts:\n{}".format(shorts)
+		signal = (stock_order=="SELL")*-1.0 + (stock_order=="BUY")
+		own0 = own.copy()
+		own.ix[date, sym] = n_shares*signal
+# 		own.ix[:date, sym] = own.ix[:date, sym].ffill()
+# 		own.ix[date, sym] += own.shift(1).fillna(0.0).ix[date, sym]
+		temp = own.ix[date, :]
+		own.ix[date,sym] += prev_trade[sym]
+		prev_trade = temp
+		longs0 = longs.copy()
+		shorts0 = shorts.copy()
+		cash0 = cash
 
+		longs = np.sum(own[own>=0].fillna(0)*portvals)
+##		print "Longs", longs
+		shorts = np.sum(own[own<0].fillna(0)*portvals)
+##		print "Shorts", shorts
+		cash += np.sum(on_hand.ix[date,:])
+## 		print "Cash", cash
+		leverage = (np.sum(longs) + np.sum(abs(shorts))) / (np.sum(longs) - np.sum(abs(shorts)) + cash)
+		print "Leverage =", leverage
+		if leverage >= 2.0:
+                        print "LEVERAGE REACHED"
+			shorts = shorts0.copy()
+			longs = longs0.copy()
+			cash = cash0
+			own = own0.copy()
+			continue
+	own = own.fillna(0)
+	print "Number different", np.sum((own.fillna(0)!=portfolio.fillna(0)))
 	
 	cash = np.sum(on_hand, axis=1)
-
+# 	print cash
+##	portfolio=own.copy()
 	for i in range(on_hand.shape[0]):
 		if i==0:
 			cash.iloc[i] += start_val
 		else:
 			cash.iloc[i] += cash.iloc[i-1]
 			portfolio.iloc[i] += portfolio.iloc[i-1]
-
+# 	print portfolio
+# 	portfolio = own
+##        print own[own!=portfolio]
+##	print portfolio[own!=portfolio]
 	portfolio = portfolio*portvals
 	portfolio = np.sum(portfolio, axis=1)
 	
@@ -132,14 +169,12 @@ def test_code():
 	# note that during autograding his function will not be called.
 	# Define input parameters
 
-	of = "./orders/orders-test.csv"
- 	of = "./orders/orders-short.csv"
- 	of = "./orders/orders.csv"
 	of = "./orders/orders2.csv"
 	of = "./orders/orders3.csv"
-	of = "./orders/orders-leverage-1.csv"
-##	of = "./orders/orders-leverage-2.csv"
 ##	of = "./orders/orders-leverage-3.csv"
+#	  of = "./orders/orders-test.csv"
+# 	of = "./orders/orders-short.csv"
+# 	of = "./orders/orders.csv"
 	sv = 1000000
 
 	# Process orders

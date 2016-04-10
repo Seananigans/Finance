a1603a9b5a10c1fe66de73369090438562f68ffd
Seananigans <sph99@comcast.net>
2016-04-10 18:49:35 -0400

    Implemented function for plotting bollinger orders
    need to separate plotting and bollinger orders

--- ml4t/Mc2_p2/bollinger_strategy.py
+++ ml4t/Mc2_p2/bollinger_strategy.py
@@ -17,12 +17,73 @@ def bollinger_bands(data, window=20):
 	upper = pd.DataFrame( sma + 2*pd.rolling_std(data, window=window) )
 	lower = pd.DataFrame( sma - 2*pd.rolling_std(data, window=window) )
 	
-	sma.columns = ["_SMA"]
-	upper.columns = ["_UPPER"]
-	lower.columns = ["_LOWER"]
-	df = sma.join(upper).join(lower)
+	sma.columns = ["SMA"]
+	upper.columns = ["UPPER"]
+	lower.columns = ["LOWER"]
+	df = data.join(sma.join(upper).join(lower))
 	return df
 
+def bollinger_plot(data):
+	b_bands = bollinger_bands(data)
+	sym = data.columns.values[0]
+	symbol=data
+	b_bands[[sym,"SMA","UPPER","LOWER"]].plot(color=['b','y','c','c'])
+	handles, labels = plt.gca().get_legend_handles_labels()
+	handles = handles[:-1]
+	labels = labels[:-1]
+	labels[-1] = "Bollinger Bands"
+	plt.legend(handles, labels, loc="upper left")
+	
+	position = None
+	for i in range(1,b_bands.shape[0]):
+		#Long Entry Signal
+		below_lower_before = (data.iloc[i-1]<b_bands["LOWER"].iloc[i-1]).values
+		above_lower_now = (data.iloc[i]>=b_bands["LOWER"].iloc[i]).values
+		long_entry = below_lower_before and above_lower_now
+		#Long Exit Signal
+		below_sma_before = (data.iloc[i-1]<b_bands["SMA"].iloc[i-1]).values
+		above_sma_now = (data.iloc[i]>=b_bands["SMA"].iloc[i]).values
+		long_exit = below_sma_before and above_sma_now and position=="Long"
+		#Short Entry Signal
+		above_upper_before = (data.iloc[i-1]>b_bands["UPPER"].iloc[i-1]).values
+		below_upper_now = (data.iloc[i]<=b_bands["UPPER"].iloc[i]).values
+		short_entry = above_upper_before and below_upper_now
+		#Short Exit Signal
+		above_sma_before = (data.iloc[i-1]>b_bands["SMA"].iloc[i-1]).values
+		below_sma_now = (data.iloc[i]<=b_bands["SMA"].iloc[i]).values
+		short_exit = above_sma_before and below_sma_now and position=="Short"
+		if position == None:
+			#Short Entry
+			if short_entry:
+				plt.axvline(x=data.index[i], color="r", linewidth=1)
+				position = "Short"
+				#issue sell order
+				print "{0},{1},SELL,100".format(data.index[i].strftime("%Y-%m-%d"),
+												sym)
+			#Long Entry
+			if long_entry:
+				plt.axvline(x=data.index[i], color="g", linewidth=1)
+				position = "Long"
+				#issue buy order
+				print "{0},{1},BUY,100".format(data.index[i].strftime("%Y-%m-%d"),
+												sym)
+		else:
+			#Short Exit
+			if short_exit:
+				plt.axvline(x=data.index[i], color="k", linewidth=1)
+				position = None
+				#issue buy order
+				print "{0},{1},BUY,100".format(data.index[i].strftime("%Y-%m-%d"),
+												sym)
+			#Long Exit
+			if long_exit:
+				plt.axvline(x=data.index[i], color="k", linewidth=1)
+				position = None
+				#issue sell order
+				print "{0},{1},SELL,100".format(data.index[i].strftime("%Y-%m-%d"),
+												sym)
+	plt.show()
+	
 def test_code():
 	# Read in adjusted closing prices for given symbols, date range
 	sd = dt.datetime(2007,12,31)
@@ -30,58 +91,8 @@ def test_code():
 	dates = pd.date_range(sd, ed)
 	syms=['IBM','GOOG','AAPL','GLD','XOM']
 	prices_all = get_data(syms, dates)
-	ibm = pd.DataFrame(prices_all[syms[0]])
-	b_bands = bollinger_bands(ibm)
-	plt.plot(prices_all[syms[0]])
-	plt.plot(b_bands.ix[:,0], color='y')
-	plt.plot(b_bands.ix[:,1], color='c')
-	plt.plot(b_bands.ix[:,2], color='c')
+	symbol = pd.DataFrame(prices_all[syms[0]])
+	bollinger_plot(symbol)
 	
-	position = None
-	for i in range(1,b_bands.shape[0]):
-		#Long Entry Signal
-		below_lower_before = (ibm.iloc[i-1]<b_bands["_LOWER"].iloc[i-1]).values
-		above_lower_now = (ibm.iloc[i]>=b_bands["_LOWER"].iloc[i]).values
-		long_entry = below_lower_before and above_lower_now
-		#Long Exit Signal
-		below_sma_before = (ibm.iloc[i-1]<b_bands["_SMA"].iloc[i-1]).values
-		above_sma_now = (ibm.iloc[i]>=b_bands["_SMA"].iloc[i]).values
-		long_exit = below_sma_before and above_sma_now and position=="Long"
-		#Short Entry Signal
-		above_upper_before = (ibm.iloc[i-1]>b_bands["_UPPER"].iloc[i-1]).values
-		below_upper_now = (ibm.iloc[i]<=b_bands["_UPPER"].iloc[i]).values
-		short_entry = above_upper_before and below_upper_now
-		#Short Exit Signal
-		above_sma_before = (ibm.iloc[i-1]>b_bands["_SMA"].iloc[i-1]).values
-		below_sma_now = (ibm.iloc[i]<=b_bands["_SMA"].iloc[i]).values
-		short_exit = above_sma_before and below_sma_now and position=="Short"
-		if position == None:
-			#Short Entry
-			if short_entry:
-				plt.axvline(x=ibm.index[i], color="r", linewidth=1)
-				position = "Short"
-				#issue sell order
-				print "{},SYMBOL,SELL,100".format(ibm.index[i].strftime("%Y-%m-%d"))
-			#Long Entry
-			if long_entry:
-				plt.axvline(x=ibm.index[i], color="g", linewidth=1)
-				position = "Long"
-				#issue buy order
-				print "{},SYMBOL,BUY,100".format(ibm.index[i].strftime("%Y-%m-%d"))
-		else:
-			#Short Exit
-			if short_exit:
-				plt.axvline(x=ibm.index[i], color="k", linewidth=1)
-				position = None
-				#issue buy order
-				print "{},SYMBOL,BUY,100".format(ibm.index[i].strftime("%Y-%m-%d"))
-			#Long Exit
-			if long_exit:
-				plt.axvline(x=ibm.index[i], color="k", linewidth=1)
-				position = None
-				#issue sell order
-				print "{},SYMBOL,SELL,100".format(ibm.index[i].strftime("%Y-%m-%d"))
-	plt.show()
-
 if __name__ == "__main__":
 	test_code()

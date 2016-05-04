f400a1363d89c98ada808a740460d682aba5dc1e
Seananigans <sph99@comcast.net>
2016-05-03 21:32:00 -0400

    Initiated potential web app

--- ml4t/indicators/Volatility.py
+++ ml4t/indicators/Volatility.py
@@ -10,7 +10,7 @@ class Volatility(object):
 
     def getIndicator(self):
     	returns = self.data/self.data.shift(1) - 1
-        vol = pd.rolling_std(returns, self.window) * np.sqrt(255)
+        vol = pd.rolling_std(returns, self.window) * np.sqrt(252)
 
         vol.columns = ["Volatility_"+x for x in vol.columns]
 
---
+++ webtest.py
@@ -0,0 +1,27 @@
+import json
+from flask import (Flask, redirect, render_template, 
+					request, url_for)
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<name>')
+def index(name="Safsdaf"):
+    return 'Hello {}'.format(name)
+    
+@app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+@app.route('/add/<float:num1>/<float:num2>')
+def add(num1, num2):
+	context = {
+				'num1':num1, 
+				'num2':num2
+				}
+	return render_template("add.html", **context)
+
+@app.route('/save', methods=["POST"])
+def save():
+	return redirect(url_for("index"))
+	
+app.run(debug=True, port=8000, host='0.0.0.0')

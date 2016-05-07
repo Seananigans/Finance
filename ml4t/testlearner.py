706145cf1a84407fc7e12ee93ac9dd73ceae5d06
Seananigans <sph99@comcast.net>
2016-05-06 22:59:12 -0400

    Refactor and Feature: Tabs to spaces and histogram plotting
    Refactor:
    Programming Environments defaulted to different versions of tabs
    (tab=4spaces machine1,
    tab=8 spaces machine 2)
    
    Feature:
    Histogram plotting function that marks and labels means and averages
    for the data. Specifically used for return variables.

--- ml4t/testlearner.py
+++ ml4t/testlearner.py
@@ -21,119 +21,128 @@ def max_normalization(trainX, testX):
     return trnX, tstX
     
 def plot_histogram(trainY):
-    mn = trainY.mean()
-    sd = trainY.std()
+    mns = trainY.mean(axis=0)
+    sds = trainY.std(axis=0)
     plt.hist(trainY)
     plt.xlabel("Daily Returns")
     plt.ylabel("Counts")
-    mean_line = plt.axvline(mn, color="k", lw=3)
-    std_line = plt.axvline(mn + sd, color="r", lw=2)
-    plt.axvline(mn - sd, color="r", lw=2)
-    plt.legend([mean_line, std_line],
-               ["Avg. {} Day\nReturn:   {}%".format(5,round(mn*100,2)),
-                "Std. Dev.\nof Returns: {}%".format(round(sd*100,2))]
+    mean_lines = [plt.axvline(mn, color="k", lw=3) for mn in mns]
+    upper_std_lines = [plt.axvline(mn + sd, color="r", lw=2) for sd in sds]
+    lower_std_lines = [plt.axvline(mn - sd, color="r", lw=2) for sd in sds]
+    std_lines = upper_std_lines+lower_std_lines
+    lines = mean_lines+std_lines
+    #Create Labels
+    labels = [None for i in lines]
+    labels[:mns.shape[0]] = [
+        "Avg. {} Day\nReturn:   {}%".format(5,
+                                            round(mn*100,2)) for mn in mns]
+    labels[mns.shape[0]:] = [
+        "Std. Dev.\nof Returns: {}%".format(round(sd*100,2)) for sd in sds]
+    plt.legend(lines,#[mean_line, std_line],
+               labels
                )
     plt.show()
     
 if __name__=="__main__":
 
-    # get actual data
-    df = pd.read_csv("simData/example.csv", index_col='Date',
-                    parse_dates=True, na_values=['nan'])
-    data = df.values
-    
-    # compute how much of the data is training and testing
-    train_rows = int(math.floor(0.6* data.shape[0]))
-    test_rows = int(data.shape[0] - train_rows)
+	# get actual data
+	df = pd.read_csv("simData/example.csv", index_col='Date',
+					parse_dates=True, na_values=['nan'])
+	data = df.values
+	
+	# compute how much of the data is training and testing
+	train_rows = int(math.floor(0.6* data.shape[0]))
+	test_rows = int(data.shape[0] - train_rows)
 
-    # separate out training and testing data
-    trainX = data[:train_rows,0:-1]
-    trainY = data[:train_rows,-1]
-    testX = data[train_rows:,0:-1]
-    testY = data[train_rows:,-1]
+	# separate out training and testing data
+	trainX = data[:train_rows,0:-1]
+	trainY = data[:train_rows,-1]
+	testX = data[train_rows:,0:-1]
+	testY = data[train_rows:,-1]
 
-    print testX.shape
-    print testY.shape
-    print "Average Return: {}".format(round( trainY.mean(),3 ))
-    plot_histogram(trainY)
-    exit()
-    trainX, testX = mean_normalization(trainX, testX)
-    
-    # create a learner and train it
-##    learners = [lrl.LinRegLearner(verbose = True), # create a LinRegLearner
-##                knn.KNNLearner(k=6, verbose = True)] # create a KNNLearner
-##    
-##    learners = [lrl.LinRegLearner(verbose = True), # create a LinRegLearner
-##                knn.KNNLearner(k=6, verbose = True), # create a KNNLearner
-##                bag.BagLearner(learner = knn.KNNLearner, # create a BagLearner
-##                                kwargs = {"k":6}, 
-##                                bags = 10, 
-##                                boost = True, 
-##                                verbose = False),
-##                bag.BagLearner(learner = lrl.LinRegLearner, # create a BagLearner
-##                                kwargs = {}, 
-##                                bags = 10, 
-##                                boost = True, 
-##                                verbose = False)]
-    
-    learners = [knn.KNNLearner(k=i) for i in range(1,25)]
-    
-    cors, rmsestrain, rmsestest = [], [], []
-    for i, learner in enumerate(learners):
-        print learner.name
-        learner.addEvidence(trainX, trainY) # train it
+	print testX.shape
+	print testY.shape
+	print "Average Return: {}".format(round( trainY.mean(),3 ))
+# 	plot_histogram(trainY)
+	
+	trainX, testX = mean_normalization(trainX, testX)
+	
+	# create a learner and train it
+# 	learners = [lrl.LinRegLearner(verbose = True), # create a LinRegLearner
+# 				knn.KNNLearner(k=6, verbose = True)] # create a KNNLearner
+   
+	learners = [lrl.LinRegLearner(verbose = True), # create a LinRegLearner
+			   knn.KNNLearner(k=6, verbose = True), # create a KNNLearner
+			   bag.BagLearner(learner = knn.KNNLearner, # create a BagLearner
+							   kwargs = {"k":6}, 
+							   bags = 10, 
+							   boost = True, 
+							   verbose = False),
+			   bag.BagLearner(learner = lrl.LinRegLearner, # create a BagLearner
+							   kwargs = {}, 
+							   bags = 10, 
+							   boost = True, 
+							   verbose = False)]
+	
+# 	learners = [knn.KNNLearner(k=i) for i in range(1,25)]
+	
+	cors, rmsestrain, rmsestest = [], [], []
+	for i, learner in enumerate(learners):
+		print learner.name
+		learner.addEvidence(trainX, trainY) # train it
 
-        # evaluate in sample
-        predYtrain = learner.query(trainX) # get the predictions
-        rmse = math.sqrt(((trainY - predYtrain) ** 2).sum()/trainY.shape[0])
-        print
-        print "In sample results"
-        print "RMSE: ", rmse
-        c = np.corrcoef(predYtrain, y=trainY)
-        print "corr: ", c[0,1]
-        rmsestrain.append(rmse)
+		# evaluate in sample
+		predYtrain = learner.query(trainX) # get the predictions
+		print predYtrain.shape
+		rmse = math.sqrt(((trainY - predYtrain) ** 2).sum()/trainY.shape[0])
+		print
+		print "In sample results"
+		print "RMSE: ", rmse
+		c = np.corrcoef(predYtrain, y=trainY)
+		print "corr: ", c[0,1]
+		rmsestrain.append(rmse)
 
-        # evaluate out of sample
-        predY = learner.query(testX) # get the predictions
-        rmse = math.sqrt(((testY - predY) ** 2).sum()/testY.shape[0])
-        print
-        print "Out of sample results"
-        print "RMSE: ", rmse
-        c = np.corrcoef(predY, y=testY)
-        print "corr: ", c[0,1]
-        print
-        cors.append(c[0,1])
-        rmsestest.append(rmse)
-        predicted = pd.DataFrame(predY,
-                           columns=["Predicted"],
-                           index=df.ix[train_rows:,-1].index)
-        predicted = predicted.join(pd.DataFrame(testY,
-                           columns=["Actual"],
-                           index=df.ix[train_rows:,-1].index))
-        if i%5==0:
-            plt.figure(1)
-            plt.subplot(211)
-            pre, = plt.plot(predicted[['Predicted']])
-            act, = plt.plot(predicted[['Actual']])
-            plt.legend([pre, act], ["Predicted", "Actual"])
-            plt.xlabel("Date")
-            plt.ylabel("Returns")
-            plt.subplot(212)
-            plt.scatter(predY, testY)
-            plt.xlabel("Predicted Returns")
-            plt.ylabel("Actual Returns")
-            plt.show()
-    
-    plt.plot(range(len(cors)), cors)
-    plt.ylabel("Correlation")
-    plt.xlabel("Model Complexity")
-    plt.show()
-    
-    # Plot testing & training error on the same plot to 
-    # show how error behaves with different models.
-    testerr, = plt.plot(range(len(cors)), rmsestest, label="Test Error")
-    trainerr, = plt.plot(range(len(cors)), rmsestrain, label="Training Error")
-    plt.legend([testerr,trainerr], ["Test Error", "Train Error"])
-    plt.xlabel("Model Complexity")
-    plt.ylabel("RMSE")
-    plt.show()
+		# evaluate out of sample
+		predY = learner.query(testX) # get the predictions
+		rmse = math.sqrt(((testY - predY) ** 2).sum()/testY.shape[0])
+		print
+		print "Out of sample results"
+		print "RMSE: ", rmse
+		c = np.corrcoef(predY, y=testY)
+		print "corr: ", c[0,1]
+		print
+		cors.append(c[0,1])
+		rmsestest.append(rmse)
+		predicted = pd.DataFrame(predY,
+						   columns=["Predicted"],
+						   index=df.ix[train_rows:,:].index)
+		predicted = predicted.join(pd.DataFrame(testY,
+						   columns=["Actual"],
+						   index=df.ix[train_rows:,:].index))
+		if i%5==0:
+			plt.figure(1)
+			plt.subplot(211)
+			pre, = plt.plot(predicted[['Predicted']])
+			act, = plt.plot(predicted[['Actual']])
+			plt.legend([pre, act], ["Predicted", "Actual"])
+			plt.xlabel("Date")
+			plt.ylabel("Returns")
+			plt.subplot(212)
+			plt.scatter(predY, testY)
+			plt.xlabel("Predicted Returns")
+			plt.ylabel("Actual Returns")
+			plt.show()
+	
+	plt.plot(range(len(cors)), cors)
+	plt.ylabel("Correlation")
+	plt.xlabel("Model Complexity")
+	plt.show()
+	
+	# Plot testing & training error on the same plot to 
+	# show how error behaves with different models.
+	testerr, = plt.plot(range(len(cors)), rmsestest, label="Test Error")
+	trainerr, = plt.plot(range(len(cors)), rmsestrain, label="Training Error")
+	plt.legend([testerr,trainerr], ["Test Error", "Train Error"])
+	plt.xlabel("Model Complexity")
+	plt.ylabel("RMSE")
+	plt.show()

70c5d2a53352b4a96215ca61cbae6bf60e90829b
Seananigans <sph99@comcast.net>
2016-05-10 23:46:08 -0400

    Test: Print Coefficients
    Print coefficients to determine the effects of multi-collinearity on
    output.

--- ml4t/learners/LinRegLearner.py
+++ ml4t/learners/LinRegLearner.py
@@ -30,6 +30,7 @@ class LinRegLearner(object):
         @param points: should be a numpy array with each row corresponding to a specific query.
         @returns the estimated values according to the saved model.
         """
+        print self.model_coefs
         return (self.model_coefs[:-1] * points).sum(axis = 1) + self.model_coefs[-1]
 
 if __name__=="__main__":

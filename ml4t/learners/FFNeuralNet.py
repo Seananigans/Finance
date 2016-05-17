11d8567a0db6d86b8fd5252798dd7ffdf9a14558
Seananigans <sph99@comcast.net>
2016-05-16 20:03:28 -0400

    Feature: Add Simple Feed Forward Neural Net

---
+++ ml4t/learners/FFNeuralNet.py
@@ -0,0 +1,219 @@
+"""Numpy implementation of a basic neural network."""
+
+import numpy as np
+import json
+import random
+import sys
+np.seterr(invalid='ignore')
+
+class CrossEntropyCost(object):
+    """Cross-Entropy Cost function and delta for output layer."""
+    @staticmethod
+    def fn(a, y):
+        return np.sum( np.nan_to_num( -y*np.log(a)-(1-y)*np.log(1-a) ) )
+
+    @staticmethod
+    def delta(z, a, y):
+        return (a-y)
+
+class QuadraticCost(object):
+    """Quadratic Cost function and delta for output layer."""
+    @staticmethod
+    def fn(a, y):
+        return 0.5*np.linalg.norm(a-y)**2
+
+    @staticmethod
+    def delta(z, a, y):
+        return (a-y)*l_relu(z, derivative=True)
+
+class LeakyReLU(object):
+    """Leaky Rectified Linear Unit."""
+    @staticmethod
+    def fn(z):
+        gt_zero = (z>0)
+        leq_zero = (z<=0)
+        return np.multiply(z, gt_zero+leq_zero*0.01)
+
+    @staticmethod
+    def prime(z):
+        return np.maximum(0.01, z>0)
+
+class Sigmoid(object):
+    """Leaky Rectified Linear Unit."""
+    @staticmethod
+    def fn(z):
+        return np.multiply(z, gt_zero+leq_zero*0.01)
+
+    @staticmethod
+    def prime(z):
+        return fn(z)*(1-fn(z))
+
+class Network(object):
+
+    def __init__(self, sizes, cost = CrossEntropyCost):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.default_weight_initializer()
+        self.cost = cost
+        
+    def default_weight_initializer(self):
+        self.biases = [np.random.randn(y,1) for y in self.sizes[1:]]
+        self.weights = [np.random.randn(y, x)/np.sqrt(x)
+                        for x, y in zip(self.sizes[:-1], self.sizes[1:])]
+        self.v_biases = [np.zeros((y, 1)) for y in self.sizes[1:]]
+        self.v_weights = [np.zeros((y, x))
+                         for x, y in zip(self.sizes[:-1], self.sizes[1:])]
+    
+    def feedforward(self, a):
+        """Return the output of the network if "a" is input."""
+        for b, w in zip(self.biases, self.weights):
+            a = l_relu(np.dot(w, a)+b)
+        return a
+
+    def SGD(self, training_data, epochs,
+            mini_batch_size, eta,
+            lmbda=0.0, test_data=None):
+        prev_acc = 0.0
+        if test_data: n_test = len(test_data)
+        n = len(training_data)
+        for j in xrange(epochs):
+            if eta<1./1024: break
+            random.shuffle(training_data)
+            mini_batches = [
+                training_data[k:k+mini_batch_size]
+                for k in xrange(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta, lmbda, n)
+            new_acc = self.evaluate(test_data)
+            if prev_acc>new_acc:
+                eta *= 0.5
+            else:
+                eta *= 1.1
+            prev_acc = new_acc
+            if test_data:
+                print "Epoch {0}: {1} / {2}".format(
+                    j, new_acc, n_test)
+            else:
+                print "Epoch {0} complete".format(j)
+
+    def update_mini_batch(self, mini_batch, eta, lmbda, n, mu=0.9):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            #Nesterov Momentum
+            # v' = mu*v - eta*nabla_Cost( w+mu*v )
+            old_weights = [w for w in self.weights]
+            old_biases = [b for b in self.biases]
+            self.weights = [w + mu*vw
+                              for w, vw in zip(self.weights, self.v_weights)]
+            self.biases = [b + mu*vb
+                              for b, vb in zip(self.biases, self.v_biases)]
+            #end Nesterov
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            #Return weights to original form
+            self.weights = [w for w in old_weights]
+            self.biases = [b for b in old_biases]
+            #end modifications
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        #Momentum method
+        # v' = mu*v - (eta/len(mini_batch))*nw
+        # w' = w + v'
+        self.v_weights = [mu*vw - (eta/len(mini_batch))*nw
+                        for vw, nw in zip(self.v_weights, nabla_w)]
+        self.v_biases = [mu*vb-(eta/len(mini_batch))*nb
+                       for vb, nb in zip(self.v_biases, nabla_b)]
+        #end Momentum
+        self.weights = [(1-eta*(lmbda/n))*w + vw
+                        for w, vw in zip(self.weights, self.v_weights)]
+        self.biases = [b + vb
+                       for b, vb in zip(self.biases, self.v_biases)]
+
+    def backprop(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        #feedforward
+        activation = x
+        activations = [x] # list to store all the activations, layer by layer
+        zs = [] # list to store all the z vectors, layer by layer
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation)+b
+            zs.append(z)
+            activation = l_relu(z)
+            activations.append(activation)
+        #backward pass
+        delta = (self.cost).delta(zs[-1], activations[-1], y)
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        for l in xrange(2, self.num_layers):
+            z = zs[-l]
+            sp = l_relu(z, derivative=True)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+        return (nabla_b, nabla_w)
+
+    def evaluate(self, test_data):
+        test_results = [(np.argmax(self.feedforward(x)),y)
+                         for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+                        
+    def cost_derivative(self, output_activations, y):
+        return (output_activations-y)
+
+    def save(self, filename):
+        """Save the neural network to the file ``filename``."""
+        data = {"sizes": self.sizes,
+                "weights": [w.tolist() for w in self.weights],
+                "biases": [b.tolist() for b in self.biases],
+                "cost": str(self.cost.__name__)}
+        f = open(filename, "w")
+        json.dump(data, f)
+        f.close()
+
+#### Loading a Network
+def load(filename):
+    """Load a neural network from the file ``filename``.  Returns an
+    instance of Network.
+    """
+    f = open(filename, "r")
+    data = json.load(f)
+    f.close()
+    cost = getattr(sys.modules[__name__], data["cost"])
+    net = Network(data["sizes"], cost=cost)
+    net.weights = [np.array(w) for w in data["weights"]]
+    net.biases = [np.array(b) for b in data["biases"]]
+    return net
+
+def l_relu(z, derivative=False):
+    """Calculate the Rectified Linear Unit for a given input."""
+    if not derivative:
+        gt_zero = (z>0)
+        leq_zero = (z<=0)
+        return np.multiply(z, gt_zero+leq_zero*0.01)
+    else:
+        gt_zero = (z>0)
+        return np.maximum(0.01, gt_zero)
+    
+##from neural_networks_and_deep_learning_master.src import mnist_loader
+##training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+##
+##filename = "test232342.txt"
+##try:
+##    net = load(filename)
+##except IOError:
+##    feature_size = len(training_data[0][0]) #784
+##    net = Network([feature_size, 30, 10], cost=QuadraticCost)
+##    
+##print len(training_data),
+##print training_data[0][0].shape
+##print "There are {} layers in this network.".format(net.num_layers)
+##print "The sizes of the layers are {}.".format(net.sizes)
+##net.SGD(training_data,
+##        epochs = 16, #Of times through training set
+##        mini_batch_size = 5,
+##        eta = 0.01, #Initial Learning Rate
+##        lmbda = 0.0, #Regularization parameter
+##        test_data=test_data)
+##
+##net.save(filename)
--- ml4t/test.py
+++ ml4t/test.py
@@ -1,7 +1,157 @@
-print """
-
-
-Moved create_training_data to util.py
-
-
 """
+Test a learner.  (c) 2015 Tucker Balch
+"""
+
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import pandas as pd
+from learners import LinRegLearner as lrl
+from learners import KNNLearner as knn
+from learners import BagLearner as bag
+from learners import ANNRegLearner as net
+
+def mean_normalization(trainX, testX):
+    trnX = ( trainX - trainX.mean(axis=0) )/trainX.std(axis=0)
+    tstX = ( testX - trainX.mean(axis=0) )/trainX.std(axis=0)
+    return trnX, tstX
+    
+def max_normalization(trainX, testX):
+    trnX = trainX / trainX.max(axis=0)
+    tstX = testX / trainX.max(axis=0)
+    return trnX, tstX
+    
+def plot_histogram(trainY):
+    mns = trainY.mean(axis=0)
+    sds = trainY.std(axis=0)
+    plt.hist(trainY)
+    plt.xlabel("Daily Returns")
+    plt.ylabel("Counts")
+    mean_lines = [plt.axvline(mn, color="k", lw=3) for mn in mns]
+    upper_std_lines = [plt.axvline(mn + sd, color="r", lw=2) for sd in sds]
+    lower_std_lines = [plt.axvline(mn - sd, color="r", lw=2) for sd in sds]
+    std_lines = upper_std_lines+lower_std_lines
+    lines = mean_lines+std_lines
+    #Create Labels
+    labels = [None for i in lines]
+    labels[:mns.shape[0]] = [
+        "Avg. {} Day\nReturn:   {}%".format(5,
+                                            round(mn*100,2)) for mn in mns]
+    labels[mns.shape[0]:] = [
+        "Std. Dev.\nof Returns: {}%".format(round(sd*100,2)) for sd in sds]
+    plt.legend(lines,#[mean_line, std_line],
+               labels)
+    plt.show()
+    
+if __name__=="__main__":
+
+	# get actual data
+	filename= "simData/example.csv"
+	
+	df = pd.read_csv(filename, index_col='Date',
+					parse_dates=True, na_values=['nan'])
+					
+	data = df.values
+	cols = [col for col in df.columns if not col.startswith("Returns")]
+	outcome = [col for col in df.columns if col.startswith("Returns")]
+	
+	# compute how much of the data is training and testing
+	train_rows = int(math.floor(0.6* df.shape[0]))
+	test_rows = int(df.shape[0] - train_rows)
+	
+	# separate out training and testing data
+	trainX = df.ix[:train_rows,0:-1]
+	trainY = df.ix[:train_rows,-1]
+	testX = df.ix[train_rows:,0:-1]
+	testY = df.ix[train_rows:,-1]
+
+	print testX.shape
+	print testY.shape
+	print "Average Training Return: {}".format(round( trainY.mean(),3 ))
+	print "Average Training Return: {}".format(round( testY.mean(),3 ))
+	err = np.zeros(testY.shape)
+	for i in range(err.shape[0]):
+            err[i] = trainY.mean()
+	print "RMSE of Train Average on Test Set: {}".format(
+            math.sqrt(((testY - err) ** 2).sum()/testY.shape[0])
+            )
+	err[0] += 0.00000001
+	
+	for i in range(trainX.shape[1]):
+            print np.corrcoef(trainX.ix[:,i], trainY)[0][1]
+# 	plot_histogram(trainY)
+	
+	trainX, testX = mean_normalization(trainX, testX)
+	
+	# create a learner and train it
+	learners = [bag.BagLearner(learner = lrl.LinRegLearner,# knn.KNNLearner, #create a BagLearner
+                                   kwargs = {},#{"k":3}, #
+                                   bags = i,
+                                   boost = True,
+                                   verbose = False) for i in range(5,100,5)]
+	learners = [net.ANNRegLearner([trainX.shape[1],100,1])]
+    
+	cors, rmsestrain, rmsestest = [], [], []
+	for i, learner in enumerate(learners):
+		print learner.name
+		learner.addEvidence(trainX.values, trainY) # train it
+
+		# evaluate in sample
+		predYtrain = learner.query(trainX) # get the predictions
+		rmse = math.sqrt(((trainY - predYtrain) ** 2).sum()/trainY.shape[0])
+		print
+		print "In sample results"
+		print "RMSE: ", rmse
+		c = np.corrcoef(predYtrain, y=trainY)
+		print "corr: ", c[0,1]
+		rmsestrain.append(rmse)
+
+		# evaluate out of sample
+		predY = learner.query(testX) # get the predictions
+		rmse = math.sqrt(((testY - predY) ** 2).sum()/testY.shape[0])
+		print
+		print "Out of sample results"
+		print "RMSE: ", rmse
+		c = np.corrcoef(predY, y=testY)
+		print "corr: ", c[0,1]
+		print
+		cors.append(c[0,1])
+		rmsestest.append(rmse)
+		predicted = pd.DataFrame(predY,
+                                         columns=["Predicted"],
+                                         index=df.ix[train_rows:,:].index)
+		predicted = predicted.join(testY)
+		
+		if i%5==0:
+			plt.figure(1)
+			plt.subplot(211)
+			pre, = plt.plot(predicted[['Predicted']])
+			act, = plt.plot(predicted[outcome])
+			plt.legend([pre, act], ["Predicted", "Actual"])
+			plt.xlabel("Date")
+			plt.ylabel("Returns")
+			plt.subplot(212)
+			plt.scatter(predY, testY)
+			plt.xlabel("Predicted Returns")
+			plt.ylabel("Actual Returns")
+			plt.show()
+		df1 = pd.DataFrame(testX,
+						   columns=cols,
+						   index=df.ix[train_rows:,:].index)
+		df1 = df1.join(predicted)
+		df1.to_csv("test.csv", index_label="Date")
+	
+	if len(learners)>4:
+		plt.plot(range(len(cors)), cors)
+		plt.ylabel("Correlation")
+		plt.xlabel("Model Complexity")
+		plt.show()
+	
+	# Plot testing & training error on the same plot to 
+	# show how error behaves with different models.
+	testerr, = plt.plot(range(len(cors)), rmsestest, label="Test Error")
+	trainerr, = plt.plot(range(len(cors)), rmsestrain, label="Training Error")
+	plt.legend([testerr,trainerr], ["Test Error", "Train Error"])
+	plt.xlabel("Model Complexity")
+	plt.ylabel("RMSE")
+	plt.show()

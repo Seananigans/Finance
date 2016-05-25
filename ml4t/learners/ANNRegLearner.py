a03a1f6aa4e9e1d888ac80be2542b40be54b8aee
Seananigans <sph99@comcast.net>
2016-05-24 21:35:03 -0400

    Features: Changed name, Removed data processing, Added Regularization, Added output dependent models
    Changed name to include the regularization parameter. This makes it
    easier to identify individual networks in a list when optimizing.
    
    Removed data processing because regularization was added.
    
    Added Regularization to initialization of the class to follow the
    format of previous learner models.
    
    Added output dependent models to automatically build models if the
    output is returns rather than prices, and vice versa.

--- ml4t/learners/ANNRegLearner.py
+++ ml4t/learners/ANNRegLearner.py
@@ -1,40 +1,48 @@
-##"""
-##A wrapper for neural network regression. 
-##"""
-##print "Not currently in production. Sorry"
-##exit()
+"""
+A wrapper for neural network regression. 
+"""
+
 
 import numpy as np
 from NeuralNet import *
 
 class ANNRegLearner(object):
 
-    def __init__(self, sizes=[], verbose = False):
-        self.name = "Neural net Regression Learner"
+    def __init__(self, sizes=[], lmbda=0.0, verbose = False):
+        self.name = "Neural net Regression Learner: lambda={}".format(lmbda)
+        self.lmbda = lmbda
         # pass # move along, these aren't the drones you're looking for
 
-    def addEvidence(self,dataX,dataY, use_trained=True):
+    def addEvidence(self,dataX,dataY, use_trained=False):
         """
         @summary: Add training data to learner
         @param dataX: X values of data to add
         @param dataY: the Y training values
         """
-        dataX = dataX[:,6:]
         feature_size = dataX.shape[1]
         dataY =  dataY.reshape(dataY.shape[0], 1)
         output_size = dataY.shape[1]
-        
-        filename = "stocknet.txt"
+        # Create potential network
+        sizes = [feature_size, 50, 50, output_size]
+        if dataY.mean()<5.0:
+        	activations = [Tanh for i in sizes[1:]]
+        else:
+        	activations = [ReLU for i in sizes[1:]]
+        net = Network(
+        		sizes = sizes, 
+            	activations = activations,
+            	lmbda = self.lmbda
+            	)
+        # Load saved network for continued training?
+        filename = "network_models/stocknet.txt"
         if use_trained:
             try:
                self.network = load(filename)
             except IOError:
-               self.network = Network(sizes = [feature_size, 20, 10, output_size], 
-               									activations=[ReLU, ReLU, ReLU])
+               self.network = net
         else:
-            self.network = Network(sizes = [feature_size, 20, 10, output_size], 
-            								activations=[Tanh, Tanh, Tanh])
-        self.network.sgd(dataX, dataY, lmbda=0.0)
+            self.network = net
+        self.network.sgd(dataX, dataY)
         self.network.save(filename)
         
     def query(self,points):
@@ -44,7 +52,6 @@ class ANNRegLearner(object):
         @returns the estimated values according to the saved model.
         """
         points = points.values
-        points = points[:,6:]
         return self.network.forward(points)
 
 if __name__=="__main__":

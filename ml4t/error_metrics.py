6a4935192315d1ce39d144c3f5d6de4c440c0737
Seananigans <sph99@comcast.net>
2016-06-01 23:33:42 -0400

    Feature: Added error metric functions

---
+++ ml4t/error_metrics.py
@@ -0,0 +1,10 @@
+import math
+import numpy as np
+
+
+def rmse(expected, predictions):
+	"""@parameters: expected values and predicted values."""
+	return math.sqrt(((expected - predictions) ** 2).sum()/expected.shape[0])
+
+def mape(expected, predictions):
+	return (np.abs(expected - predictions)/expected).mean()

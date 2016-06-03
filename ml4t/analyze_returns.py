6a4375db07c3b98f0cbd2ef17dfbf1c68a04b423
Seananigans <sph99@comcast.net>
2016-06-02 23:55:16 -0400

    Feature: Created spy.py to analyze learners predictions
    Created spy.py to analyze learners predictions by running a strategy
    with based on the predictions on all companies in the S&P 500

---
+++ ml4t/analyze_returns.py
@@ -0,0 +1,28 @@
+import re
+import numpy as np
+fhand = open('test.txt')
+
+port_returns = []
+spy_returns = []
+port_sharpe = []
+spy_sharpe = []
+
+for line in fhand:
+	if re.search("Cumulative Return of Fund:", line):
+		returns = float(line.split()[-1])
+		port_returns.append(returns)
+	elif re.search("Cumulative Return of ", line):
+		returns = float(line.split()[-1])
+		spy_returns.append(returns)
+	if re.search("Sharpe Ratio of Fund:", line):
+		returns = float(line.split()[-1])
+		port_sharpe.append(returns)
+	elif re.search("Sharpe Ratio of ", line):
+		returns = float(line.split()[-1])
+		spy_sharpe.append(returns)
+
+print "Average Portfolio Return: {}".format(np.mean(port_returns))
+print "Average Portfolio Sharpe Ratio: {}".format(np.nanmean(port_sharpe))
+print "Average SPY Return: {}".format(np.mean(spy_returns))
+print "Average SPY Sharpe Ratio: {}".format(np.nanmean(spy_sharpe))
+fhand.close()
--- ml4t/spy.py
+++ ml4t/spy.py
@@ -1,9 +1,15 @@
 import os
 import pandas as pd
 
-spy_list = ["AAPL","ABT","ABBV","ACN","ADBE","ATVI","AYI","MMM","IBM"]
+#########
+print """
+Sorry. Currently not in use.
+"""
+exit()
+#########
+
 fhand = pd.read_csv("spy_list.csv")
 spy_list = list(fhand.Symbols)
 for i in spy_list:
-	os.system('python learner_strategy.py {} 10 0.04'.format(i))
+	os.system('python learner_strategy.py {} 5 0.05'.format(i))
 	os.system('python marketsim.py')

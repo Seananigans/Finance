60db11e697ca5b57e9f138d7d9371ebbac6d12d3
Seananigans <sph99@comcast.net>
2016-06-04 22:47:09 -0400

    Feature: Added correlation analysis
    Added correlation analysis to consider using other stock prices and
    their indicators as indicators for the return on a given stock,
    provided the other stocks are highly correlated.

---
+++ ml4t/cor_spy.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from pandas.tseries.offsets import BDay
+import numpy as np
+import datetime as dt
+import matplotlib.pyplot as plt
+import math, os, sys
+
+# Import dataset retrieval
+from dataset_construction import create_input, create_output
+# Import normalization
+from normalization import mean_normalization
+
+fhand = pd.read_csv("spy_list.csv")
+spy_list = list(fhand.Symbols)
+results = pd.DataFrame()
+results = results.append({'Date': np.nan, "Return Date": np.nan, 'Symbol': np.nan, 'Return': np.nan, 'Test Error (RMSE)': np.nan}, ignore_index=True)
+
+df = create_input(spy_list[0])
+for sym in spy_list[1:]:
+	if sym == "ADT": continue
+	df = df.join(create_input(sym, indicators=[], store=False))
+
+print df.corr()

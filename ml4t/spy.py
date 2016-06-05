5d3c21181292db2b4f06d23391fa77be0b4bd6b4
Seananigans <sph99@comcast.net>
2016-06-04 22:55:00 -0400

    Feature: Added more arguments to programs being run

--- ml4t/spy.py
+++ ml4t/spy.py
@@ -1,15 +1,20 @@
 import os
 import pandas as pd
+from random import sample
 
-#########
-print """
-Sorry. Currently not in use.
-"""
-exit()
-#########
+###########
+##print """
+##Sorry. Currently not in use.
+##"""
+##exit()
+###########
 
 fhand = pd.read_csv("spy_list.csv")
 spy_list = list(fhand.Symbols)
-for i in spy_list:
-	os.system('python learner_strategy.py {} 5 0.05'.format(i))
-	os.system('python marketsim.py')
+# spy_list = sample(spy_list,50)
+
+for _, i in enumerate(spy_list):
+	# Learner Strategy args = [1]symbol [2]horizon [3]threshold [4]num_shares [5]shorting?
+	os.system('python learner_strategy.py {} 5 0.01 10 F'.format(i))
+	# Market Simulator args = [1]create a plot [2]portfolio start value
+	os.system('python marketsim.py T 1000')
